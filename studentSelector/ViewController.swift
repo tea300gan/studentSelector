f0646079a1c26469e444faab70cc40af9a06a6ef
TeaganRett <teagan.rett@gmail.com>
2021-03-22 08:35:44 -0500

    FINAL FIXES

--- studentSelector/ViewController.swift
+++ studentSelector/ViewController.swift
@@ -9,18 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var studentDisplay: UILabel!
+    
+    var classList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func generateStudent(_ sender: UIButton) {
-        let studentSelected = "Joe"
+        let x = Int.random(in: 0...(classList.count-1))
+        let studentSelected = classList[x]
         studentDisplay.text = studentSelected
     }
     
     @IBAction func editList(_ sender: UIButton) {
         performSegue(withIdentifier: "goToEdit", sender: nil)
     }
+    
+    @IBAction func unwind(_ seg: UIStoryboardSegue){
+        let classNames = seg.source as! ViewController2
+        classList = classNames.studentList
+    }
+
     
 }
 
--- studentSelector/ViewController2.swift
+++ studentSelector/ViewController2.swift
@@ -7,23 +7,66 @@
 
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var newEntery: UITextField!
+    @IBOutlet weak var listDisplay: UITableView!
     
     var studentList = ["Braddly", "Emma", "Cooper", "Nick"]
     
+    let defults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        listDisplay.delegate = self
+        listDisplay.dataSource = self
+        if let list = defults.object(forKey: "students"){
+            studentList = list as! [String]
+        }
         // Do any additional setup after loading the view.
     }
     
     @IBAction func addName(_ sender: UIButton) {
-        studentList.append(String(newEntery.text!))
+        var check = "yes"
+        for index in 0...studentList.count-1{
+            if(newEntery.text == studentList[index]){
+                check = "no"
+            }
+        }
+        if(check == "yes"){
+            studentList.append(newEntery.text!)
+            listDisplay.reloadData()
+            newEntery.text = ""
+        }  else{
+            let alert = UIAlertController(title: "Error", message: "This item already exists.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+            newEntery.text = ""
+        }
     }
     
-    
+    @IBAction func saveButton(_ sender: UIButton) {
+        defults.set(studentList, forKey: "students")
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return studentList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = studentList[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if(editingStyle == .delete){
+            studentList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
     /*
     // MARK: - Navigation
 
@@ -33,3 +76,4 @@ class ViewController2: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
